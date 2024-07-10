c86c0407901d32a8b10582c7e8ac37663675ec60
AllanLysiuk <allan.lysiuk@gmail.com>
2024-07-10 19:40:43 +0300

    Initial Commit

---
+++ TechnologiesProj/TechnologiesProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnologiesProj
+//
+//  Created by Аллан Лысюк on 10.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
